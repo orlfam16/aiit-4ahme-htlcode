fcb52a0570751fd05b1f28d9f7231f425691c28f
orlfam16 <fabian.orlitsch@gmail.com>
2020-04-24 10:44:09 +0200

    Repository aktualisiert

--- java-corona-orlitsch/src/stopwatch/Server.java
+++ java-corona-orlitsch/src/stopwatch/Server.java
@@ -3,39 +3,73 @@ package stopwatch;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Server {
     
     private ServerSocket serverSocket;
-    
-    public Server() throws IOException {
-    
-    serverSocket = new ServerSocket(8080);
-    
-    }
-    private final List<ConnectionHandler> handlers = new List<>();
+    private final List<ConnectionHandler> handlers = new ArrayList<>();
     private long timeOffset;
     private long startMillis;
     
-    public void start(int port){
+    public Server() {
+    }
     
-    
+    public void start(int port) throws IOException{
+        serverSocket = new ServerSocket(port);
+        timeOffset = 0;
+        
+        while(true){
+            final Socket clientSocket = serverSocket.accept();
+            final ConnectionHandler h = new ConnectionHandler(clientSocket);   
+            handlers.add(h);
+        } 
     }
     
     public boolean isTimeRunning(){
-        return false;
+        return startMillis > 0;
     }
     
     public long getTimeMillis(){
-        return 100;
+        return timeOffset;
     }
     
-    public static void main(String[] args) {
-        
-        new Server();
-        
+    //---------------------------------------------------------------------------
+    
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+        server.start(8080);
     }
     
+    //---------------------------------------------------------------------------
+    
+    public class ConnectionHandler extends Thread {
+    
+        private Socket socket;
+        private boolean master;
+    
+        public ConnectionHandler (Socket socket){
+            this.socket = socket;
+        }
+    
+        public boolean isClosed() {
+            return true;//um Fehlermeldung zu verhindern
+        }
+    
+        public boolean isMaster() {
+            return false; //um Fehlermeldung zu verhindern
+        }
+    
+    
+        @Override
+        public void run(){
+        }
+    
+    }
 }
+
+
+
